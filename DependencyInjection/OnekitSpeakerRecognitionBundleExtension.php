38d439a32269b6163bbfb01344374f313d2dbe08
ant <onekit@gmail.com>
2016-12-28 23:36:25 +0300

    refactoring

--- DependencyInjection/OnekitSpeakerRecognitionBundleExtension.php
+++ DependencyInjection/OnekitSpeakerRecognitionBundleExtension.php
@@ -2,10 +2,10 @@
 
 namespace Onekit\SpeakerRecognitionBundle\DependencyInjection;
 
-use \Symfony\Component\DependencyInjection\ContainerBuilder;
-use \Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use \Symfony\Component\Config\FileLocator;
-use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 
 class OnekitSpeakerRecognitionBundleExtension extends Extension
