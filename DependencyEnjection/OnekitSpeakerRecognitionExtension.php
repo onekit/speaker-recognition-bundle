f3ce545a96a37bc011b08960cfcc84727f017b62
ant <onekit@gmail.com>
2016-12-28 22:36:46 +0300

    refactoring

--- DependencyEnjection/OnekitSpeakerRecognitionExtension.php
+++ DependencyEnjection/OnekitSpeakerRecognitionExtension.php
@@ -8,16 +8,14 @@ use \Symfony\Component\Config\FileLocator;
 use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 
-class OnekitSpeakerRecognitionBundleExtension extends Extension
+class OnekitSpeakerRecognitionExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
-        $loader->load('speaker-recognition.yml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
+
 
     public function getAlias()
     {
