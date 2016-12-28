16047541d525745cd80e3dfa72f824f6bced719c
ant <onekit@gmail.com>
2016-12-29 00:14:43 +0300

    refactoring

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,7 +11,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('speaker_recognition');
+        $treeBuilder->root('speaker-recognition-bundle');
         return $treeBuilder;
     }
 }
--- OnekitSpeakerRecognitionBundle.php
+++ OnekitSpeakerRecognitionBundle.php
@@ -1,6 +1,9 @@
 <?php
 namespace Onekit\SpeakerRecognitionBundle;
+
+
 use \Symfony\Component\HttpKernel\Bundle\Bundle;
+
 class OnekitSpeakerRecognitionBundle extends Bundle
 {
 }
